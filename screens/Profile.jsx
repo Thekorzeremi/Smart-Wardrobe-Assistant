832eddef718b35f9c13725b8df2db76f7a883648
WilFriite <jnguessan2003@gmail.com>
2026-04-17 12:13:29 +0200

    feat: add location services and enhance Profile screen with user location

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,75 +1,84 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useAuth } from "../contexts/AuthContext";
+import { usePhoneLocation } from "../services/geocoding-service";
 
 export const Profile = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.avatar}>
-        <Text style={styles.avatarText}>R</Text>
-      </View>
-      <Text style={styles.name}>Remi</Text>
-      <Text style={styles.city}>Berlin, Germany</Text>
+	const add = usePhoneLocation()
 
-      <View style={styles.menu}>
-        <Text style={styles.menuItem}>Modifier le profil</Text>
-        <Text style={styles.menuItem}>Notifications</Text>
-        <Text style={styles.logout}>Deconnexion</Text>
-      </View>
-    </View>
-  );
+	const { user, signOut } = useAuth();
+	const username = user?.email?.split("@")[0];
+	const firstLetter = username?.charAt(0);
+	return (
+		<View style={styles.container}>
+			<View style={styles.avatar}>
+				<Text style={styles.avatarText}>{firstLetter.toUpperCase()}</Text>
+			</View>
+			<Text style={styles.name}>{username}</Text>
+			<Text style={styles.city}>{add?.city} - {add?.country}</Text>
+
+			<View style={styles.menu}>
+				<Text style={styles.menuItem}>Modifier le profil</Text>
+				<Text style={styles.menuItem}>Notifications</Text>
+				<TouchableOpacity style={styles.menuItem} onPress={() => console.log("signout bro")}>
+					<Text style={styles.logout}>Déconnexion</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#05070f",
-    alignItems: "center",
-    paddingTop: 92,
-    paddingHorizontal: 20,
-    paddingBottom: 110,
-  },
-  avatar: {
-    width: 104,
-    height: 104,
-    borderRadius: 52,
-    backgroundColor: "rgba(255,255,255,0.2)",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 18,
-  },
-  avatarText: {
-    color: "#f3f5ff",
-    fontSize: 38,
-    fontWeight: "700",
-  },
-  name: {
-    color: "#f3f5ff",
-    fontSize: 35,
-    fontWeight: "700",
-  },
-  city: {
-    color: "rgba(243,245,255,0.72)",
-    fontSize: 20,
-    marginTop: 4,
-  },
-  menu: {
-    width: "100%",
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: "rgba(255,255,255,0.14)",
-    backgroundColor: "rgba(132, 137, 255, 0.14)",
-    marginTop: 28,
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    gap: 14,
-  },
-  menuItem: {
-    color: "#f3f5ff",
-    fontSize: 20,
-    fontWeight: "500",
-  },
-  logout: {
-    color: "#ff5f69",
-    fontSize: 20,
-    fontWeight: "600",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#05070f",
+		alignItems: "center",
+		paddingTop: 92,
+		paddingHorizontal: 20,
+		paddingBottom: 110,
+	},
+	avatar: {
+		width: 104,
+		height: 104,
+		borderRadius: 52,
+		backgroundColor: "rgba(255,255,255,0.2)",
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: 18,
+	},
+	avatarText: {
+		color: "#f3f5ff",
+		fontSize: 38,
+		fontWeight: "700",
+	},
+	name: {
+		color: "#f3f5ff",
+		fontSize: 35,
+		fontWeight: "700",
+	},
+	city: {
+		color: "rgba(243,245,255,0.72)",
+		fontSize: 20,
+		marginTop: 4,
+	},
+	menu: {
+		width: "100%",
+		borderRadius: 20,
+		borderWidth: 1,
+		borderColor: "rgba(255,255,255,0.14)",
+		backgroundColor: "rgba(132, 137, 255, 0.14)",
+		marginTop: 28,
+		paddingVertical: 12,
+		paddingHorizontal: 16,
+		gap: 14,
+	},
+	menuItem: {
+		color: "#f3f5ff",
+		fontSize: 20,
+		fontWeight: "500",
+	},
+	logout: {
+		color: "#ff5f69",
+		fontSize: 20,
+		fontWeight: "600",
+	},
 });
