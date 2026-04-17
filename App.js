1306e57d44ccc96bdec191a95bfc947c76dda722
WilFriite <jnguessan2003@gmail.com>
2026-04-17 10:47:28 +0200

    feat: add React Query integration and handle app state changes

--- App.js
+++ App.js
@@ -4,12 +4,35 @@ import { GlassTabBar } from "./components/GlassTabBar";
 import { Home } from "./screens/Home";
 import { Profile } from "./screens/Profile";
 import { Wardrobe } from "./screens/Wardrobe";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from 'react'
+import { AppState, Platform, AppStateStatus } from 'react-native'
+import { focusManager } from '@tanstack/react-query'
 
 const Tab = createBottomTabNavigator();
 
+const qc = new QueryClient();
+
+/**
+ * 
+ * @param {AppStateStatus} status
+ */
+function onAppStateChange(status) {
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active')
+  }
+}
+
 export default function App() {
+
+	useEffect(() => {
+		const subscription = AppState.addEventListener('change', onAppStateChange)
+	
+		return () => subscription.remove()
+	}, [])
   return (
-    <NavigationContainer>
+    <QueryClientProvider client={qc}>
+			<NavigationContainer>
       <Tab.Navigator
         initialRouteName="Accueil"
         tabBar={(props) => <GlassTabBar {...props} />}
@@ -23,5 +46,6 @@ export default function App() {
         <Tab.Screen name="Profil" component={Profile} />
       </Tab.Navigator>
     </NavigationContainer>
+		</QueryClientProvider>
   );
 }
